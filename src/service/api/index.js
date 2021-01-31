61c459e9407534db6cb3570dddd8b1067e91f6fb
Serj Sidelnikov <serj2107@gmail.com>
2021-01-31 11:57:21 +0300

    API search

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -3,13 +3,15 @@
 const {Router} = require(`express`);
 const article = require(`./article`);
 const category = require(`./category`);
-const {ArticleService, CommentService, CategoryService} = require(`../data-service`);
+const search = require(`./search`)
+const {ArticleService, CommentService, CategoryService, SearchService} = require(`../data-service`);
 
 module.exports = (mockData) => {
   const app = new Router();
 
   article(app, new ArticleService(mockData), new CommentService());
   category(app, new CategoryService(mockData));
+  search(app, new SearchService(mockData));
 
   return app;
 };
---
+++ src/service/api/search.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const {Router} = require(`express`);
+const {HttpCode} = require(`../../constants`);
+
+module.exports = (app, service) => {
+  const route = new Router();
+
+  app.use(`/search`, route);
+
+  route.get(`/`, (req, res) => {
+    const {query = ``} = req.query;
+
+    if (!query) {
+      return res.status(HttpCode.BAD_REQUEST).json([]);
+    }
+
+    const searchResult = service.findAll(query);
+    const searchStatus = searchResult.length > 0 ? HttpCode.OK : HttpCode.NOT_FOUND;
+
+    return res.status(searchStatus).json(searchResult);
+  });
+};
