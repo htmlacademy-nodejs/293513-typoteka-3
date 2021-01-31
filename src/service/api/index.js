d61bfb4902eef402fa71f3badcd3b0815ed5e0da
Serj Sidelnikov <serj2107@gmail.com>
2021-01-31 11:47:05 +0300

    API category

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -2,12 +2,14 @@
 
 const {Router} = require(`express`);
 const article = require(`./article`);
-const {ArticleService, CommentService} = require(`../data-service`);
+const category = require(`./category`);
+const {ArticleService, CommentService, CategoryService} = require(`../data-service`);
 
 module.exports = (mockData) => {
   const app = new Router();
 
   article(app, new ArticleService(mockData), new CommentService());
+  category(app, new CategoryService(mockData));
 
   return app;
 };
