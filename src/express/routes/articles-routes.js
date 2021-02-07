7f095a7c22aacaba24c80eb496f455d860ba1064
Serj Sidelnikov <serj2107@gmail.com>
2021-02-07 15:30:42 +0300

    Edit article

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 
 const articlesRouter = new Router();
 
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 articlesRouter.get(`/add`, (req, res) => res.render(`new-post`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`new-post`));
+
+articlesRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [article, categories] = await Promise.all([
+    api.getArticleById(id),
+    api.getCategories(),
+  ]);
+  res.render(`new-post`, {article, categories});
+});
+
 articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
 
 module.exports = articlesRouter;
