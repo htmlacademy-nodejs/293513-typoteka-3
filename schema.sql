b4408a2acdfb1122dd80235fb0eeafca5af65418
Serj Sidelnikov <sv@spiks.ru>
2021-02-12 15:04:07 +0300

    Created schema db

---
+++ schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  password_hash varchar(255) NOT NULL,
+  avatar varchar(50) NOT NULL,
+  email varchar(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE articles(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id integer NOT NULL,
+  title varchar(255) NOT NULL,
+  picture varchar(50),
+  created_at timestamp DEFAULT current_timestamp,
+  announce text NOT NULL,
+  full_text text,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  article_id integer NOT NULL,
+  user_id integer NOT NULL,
+  text text NOT NULL,
+  created_at timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (article_id) REFERENCES articles(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE categories(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE article_category(
+  article_id integer NOT NULL,
+  category_id integer NOT NULL,
+  PRIMARY KEY (article_id, category_id),
+  FOREIGN KEY (article_id) REFERENCES articles(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
